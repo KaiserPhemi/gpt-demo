35784d231cd541d1bdb1334901c665b5bfc2914c
KaiserPhemi <kaiser.phemi@protonmail.ch>
2022-12-26 13:51:03 +0100

    feature()
    - add main styling

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import React from "react";
 import { Blog, Features, Footer, Header } from "./containers";
 
 // components
-import { Article, Brand, CTA , Feature} from "./components";
+import { Brand, CTA, Navbar } from "./components";
+
+// srtyles
+import "./app.css";
 
 // main app component
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to GPT</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Features />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
