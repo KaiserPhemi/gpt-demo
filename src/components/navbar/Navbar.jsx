a44d9f15b846c3cea9e258bfbf03af801f4d5ef0
KaiserPhemi <kaiser.phemi@protonmail.ch>
2022-12-26 14:25:39 +0100

    feature(styling)
    - add core styling
    - add assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,23 @@
 // react libraries
 import React from "react";
 
+// third-party libraires
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+
 // styles
 import "./navbar.css";
 
 // components
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img alt="logo" src="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import { createRoot } from "react-dom/client";
 // compnents
 import App from "./App";
 
+// styles
+import './index.css'
+
 // render component
 const root = createRoot(document.getElementById("main-app"));
 root.render(<App />);
+  
