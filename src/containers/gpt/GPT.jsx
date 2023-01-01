ddaceff2392dfac2ea3d4c70fa63ac14ff2f3595
KaiserPhemi <kaiser.phemi@protonmail.ch>
2023-01-01 18:37:03 +0100

    feature()
    - refactor

---
+++ src/containers/gpt/GPT.jsx
@@ -0,0 +1,24 @@
+// react libraries
+import React from "react";
+
+// components
+import { Feature } from "../../components/feature/Feature";
+
+// styles
+import "./gpt.css";
+
+// component
+const GPT = () => {
+  return (
+    <div className="gpt3__wgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__wgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__wgpt3-heading">
+        <h1></h1>
+      </div>
+    </div>
+  );
+};
+
+export default GPT;
